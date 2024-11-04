abf1a2aba29bca6b6feeff4fe53534bbf5e0af23
Patricia Alonso Ayllón <alonso.aypat22@salesianos.triana.edu>
2024-11-04 12:01:00 +0100

    nav terminado y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GasolinerasComponent } from './components/gasolineras/gasolineras.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: GasolinerasComponent},
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MaterialModule } from './modules/material/material.module';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { GasolinerasComponent } from './components/gasolineras/gasolineras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    GasolinerasComponent
   ],
   imports: [
     BrowserModule,
